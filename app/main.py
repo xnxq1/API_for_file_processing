498ebe6b61df5f1ee0e106dbd650b3ca731f1b07
xnxq1 <vasya3222@inbox.ru>
2024-08-24 23:55:04 +0300

    Добавление Celery и Redis

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from app.config import settings
+from fastapi.staticfiles import StaticFiles
 from app.user.router import router as router_auth
 from app.file.router import router as router_file
 app = FastAPI()
+app.mount('/app/static', StaticFiles(directory='app/static'), name='static')
 app.include_router(router_auth)
 app.include_router(router_file)
