62d8222ad17c1b7e892bd0816dd11d8f7398347e
xnxq1 <vasya3222@inbox.ru>
2024-08-27 02:05:35 +0300

    Вариант с обработкой через 2 процесса

--- app/tasks.py
+++ app/tasks.py
@@ -9,10 +9,8 @@ from sqlalchemy import create_engine, insert
 from app.celery_config import celery_app
 import requests
 import threading
-from pathlib import Path
-from app.config import settings
-from app.db import engine
-from app.file.models import FileUser
+
+import multiprocessing
 from app.file.repository import RepoFile
 from app.file.service import service_delete_file
 
@@ -28,7 +26,7 @@ def download_chunk(url, start, end, file_name, errors):
     except BaseException as e:
         errors.append(e)
 
-# ПОТОКИ В СРЕДНЕМ 49 СЕКУНД НА 500 МБ ФАЙЛ
+#ПОТОКИ В СРЕДНЕМ 49 СЕКУНД НА 500 МБ ФАЙЛ
 @shared_task
 def task_download_file(name, format, url, file_size, user_id, num_chunks=4):
     url_for_download = f'app/static/{user_id}_{name}.{format}'
@@ -59,6 +57,41 @@ def task_download_file(name, format, url, file_size, user_id, num_chunks=4):
         loop.run_until_complete(RepoFile.delete_file(name=name, format=format, author_id=user_id))
     else:
         loop.run_until_complete(RepoFile.change_status(True, name=name, format=format, author_id=user_id))
+
+
+# ПРОЦЕССЫ В СРЕДНЕМ 46 СЕКУНД НА 500 МБ ФАЙЛ
+# @shared_task
+# def task_download_file(name, format, url, file_size, user_id, num_chunks=4):
+#     url_for_download = f'app/static/{user_id}_{name}.{format}'
+#     errors = []
+#     with open(url_for_download, 'wb') as file:
+#         file.truncate(file_size)
+#
+#     chunk_size = file_size // num_chunks
+#
+#
+#     process = []
+#     for i in range(num_chunks):
+#         start = i * chunk_size
+#         end = start + chunk_size - 1 if i != num_chunks - 1 else ''
+#         proc = multiprocessing.Process(target=download_chunk, args=(url, start, end, url_for_download, errors))
+#         process.append(proc)
+#         proc.start()
+#
+#     # Ждем завершения всех задач
+#     for proc in process:
+#         proc.join()
+#
+#     loop = asyncio.get_event_loop()
+#     if loop.is_closed():
+#         loop = asyncio.new_event_loop()
+#         asyncio.set_event_loop(loop)
+#
+#     if errors:
+#         service_delete_file(name=name, format=format)
+#         loop.run_until_complete(RepoFile.delete_file(name=name, format=format, author_id=user_id))
+#     else:
+#         loop.run_until_complete(RepoFile.change_status(True, name=name, format=format, author_id=user_id))
 
 # СТАНДАРТНЫЙ ВАРИАНТ В СРЕДНЕМ 50 СЕКУНД НА 500 МБ ФАЙЛ
 # @shared_task
